9e418679bbfe61f5d2717f955f1908bcfe9b6d0b
santoshkaranam <karanam@iiasa.ac.at>
2023-12-26 11:55:26 +0100

    init wip

---
+++ SwashbuckleDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestFullMinimalApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Retrieves WeatherForecast
+    /// </summary>
+    /// <remarks>Awesomeness!</remarks>
+    /// <response code="200">Retrieved</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">Oops! Can't lookup your request right now</response>
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
